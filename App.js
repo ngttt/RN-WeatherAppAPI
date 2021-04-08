bbf30d127947a628090efcb021795cf3d5478de4
ngttt <1754050092tien@ou.edu.vn>
2021-04-08 15:08:23 +0700

    commit

--- App.js
+++ App.js
@@ -1,21 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { fetchLocationId, fetchWeather } from "./utils/api";
+import getImageForWeather from "./utils/getImageForWeather";
+
+import SearchInput from "./components/SearchInput";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      error: false,
+      location: "",
+      temperature: 0,
+      weather: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        weather: "Clear",
+      },
+      () => console.log(this.state)
+    );
+  }
+
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+
+    this.setState(
+      {
+        loading: true,
+      },
+      async () => {
+        try {
+          const locationId = await fetchLocationId(city);
+          const { location, weather, temperature } = await fetchWeather(
+            locationId
+          );
+
+          this.setState({
+            loading: false,
+            error: false,
+            location,
+            weather,
+            temperature,
+          });
+        } catch (e) {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        }
+      }
+    );
+  };
+
+  render() {
+    const { loading, error, location, weather, temperature } = this.state;
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weater, please try a differnt city
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)} do C`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#34495E",
+  },
+
+  imageContainer: {
+    flex: 1,
+  },
+
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover",
+  },
+
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
+    paddingHorizontal: 20,
+  },
+
+  textStyle: {
+    textAlign: "center",
+    fontFamily: "Roboto",
+    color: "white",
+  },
+
+  largeText: {
+    fontSize: 44,
+  },
+
+  smallText: {
+    fontSize: 18,
   },
 });
